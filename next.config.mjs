9790b1f85ec544b9f5d42d2da8ba23d7d4dd0515
Md Samer Ansari <smaitykhan@gmail.com>
2024-10-01 17:21:16 +0530

    vercel configuration .wasm debug

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
+const nextConfig = {
+    webpack: (config, { isServer }) => {
+      config.experiments = { asyncWebAssembly: true, layers: true };
+      config.module.rules.push({
+        test: /\.wasm$/,
+        type: 'webassembly/async',
+      });
+      return config;
+    },
+  };
+  
 export default nextConfig;
