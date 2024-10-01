d11366da640090e72ebfad3a11b12caff1801f22
Md Samer Ansari <smaitykhan@gmail.com>
2024-10-01 22:37:10 +0530

    feat: add copy-webpack-plugin and configure for tiktoken WASM file

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,22 @@
+import CopyWebpackPlugin from "copy-webpack-plugin";
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    webpack(config) {
+        config.plugins = [
+            ...config.plugins,
+            new CopyWebpackPlugin({
+                patterns: [
+                    {
+                        from: "node_modules/tiktoken/tiktoken_bg.wasm", // Adjusted path for npm
+                        to: "tiktoken_bg.wasm",
+                        toType: "file",
+                    },
+                ],
+            }),
+        ];
+        return config;
+    },
+};
 
 export default nextConfig;
