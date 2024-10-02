b534f18d3d6e592b418554145c500e9671de62a5
Md Samer Ansari <smaitykhan@gmail.com>
2024-10-02 08:49:00 +0530

    refactor: simplify webpack configuration in Next.js and remove unused files

--- next.config.js
+++ next.config.js
@@ -1,27 +1,10 @@
-const CopyPlugin = require("copy-webpack-plugin");
-
 module.exports = {
   productionBrowserSourceMaps: true,
-  webpack: (config, { isServer, dev }) => {
+  webpack: (config) => {
     config.experiments = {
       asyncWebAssembly: true,
       layers: true,
     };
-    if (isServer && dev) {
-      config.devtool = "source-map";
-      // config.plugins.push(
-      //   new CopyPlugin({
-      //     patterns: [
-      //       {
-      //         from: "./node_modules/tiktoken/lite/tiktoken_bg.wasm",
-      //         to: "tiktoken_bg.wasm",
-      //         toType: "file",
-      //       },
-      //     ],
-      //   }),
-      // );
-    }
-
     return config;
   },
   experimental: {
--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,5 @@
 /** @type {import('next').NextConfig} */
 import nextConfig from "./next.config.js";
-import webpack from "./webpack.config.mjs";
 
 nextConfig.webpack = webpack;
 
