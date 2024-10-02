73fa0a3cb300b4b363204b0eae65aa7ed7d70aed
Md Samer Ansari <smaitykhan@gmail.com>
2024-10-02 20:00:17 +0530

    feat: add Navbar, Footer, HeroSection, and GridPattern components; update package.json and .gitignore

---
+++ components/ui/Hero/HeroSection.tsx
@@ -0,0 +1,18 @@
+'use client';
+
+
+export default function HeroSection() {
+       return (
+        <div className="max-w-6xl px-4 py-8 mx-auto sm:py-24 sm:px-6 lg:px-8">
+          <div className="sm:flex sm:flex-col sm:align-center">
+            <h1 className="text-4xl font-extrabold text-white sm:text-center sm:text-6xl">
+              Aurora Ai
+            </h1>
+            <p className="max-w-2xl m-auto mt-5 text-xl text-zinc-200 sm:text-center sm:text-2xl">
+             Your One-Stop Gateway to the Ultimate Unified AI Assistant for All Your Needs.
+            </p>
+        
+          </div>
+        </div>
+    );
+  }
